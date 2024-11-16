d4f3087e14fe12967d0c399ae80b1722033076c9
Alan Rawr <alanrojaspers@gmail.com>
2024-11-16 12:47:40 -0600

    pp

--- game_jam/Scripts/knight.gd
+++ game_jam/Scripts/knight.gd
@@ -5,6 +5,8 @@ const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
 
 @onready var animated_spirte = $k_animation
+var current_state : String
+var attack_1 = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -18,22 +20,30 @@ func _physics_process(delta: float) -> void:
 	# Get input directions -1, 0, 1 -> left, center, right
 	var direction := Input.get_axis("move_left", "move_right")
 	
-	if direction > 0:
-		animated_spirte.flip_h = false
-	
-	elif direction < 0:
-		animated_spirte.flip_h = true
-	
-	var attack_1 = Input.is_key_pressed(KEY_SPACE)
-	
-	if attack_1:
-		animated_spirte.play("attack_1")
-	
-	attack_1 = false
-	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	handle_animation(direction)
+
+	
+func handle_animation(dir):
+	sprite_flip(dir)
+	
+	if attack_1 == false:
+		attack_1 = Input.is_action_pressed("attack_1")
+	
+	if attack_1:
+		animated_spirte.play("attack_1")
+	
+	else:
+		animated_spirte.play("idle")
+
+func sprite_flip(direction):
+	if direction > 0:
+		animated_spirte.flip_h = false
+	
+	elif direction < 0:
+		animated_spirte.flip_h = true
