6eb6bb58a0a0304b70bd4ca6affc1a9373c12f2e
Alan Rawr <alanrojaspers@gmail.com>
2024-11-16 14:46:19 -0600

    ...

--- game_jam/Scripts/orc.gd
+++ game_jam/Scripts/orc.gd
@@ -1,35 +1,37 @@
 extends CharacterBody2D
 
 @onready var player = get_parent().get_node( "Knight")
+@onready var sprite = $Orc_animation
 
 const life = 10
+const SPEED = 75.0
+const JUMP_VELOCITY = -100.0
 
-const SPEED = 100.0
-const JUMP_VELOCITY = -400.0
+func _ready():
+	position = Vector2(55, 0)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * delta 
 	
 	var player_position = player.position
-	var target_position = (player_position - position).normalized( )
+	var target_position = Vector2((player_position - position).normalized( ))
+	var direction = player.position.x - target_position.x 
 	
+	if (player_position.y - target_position) > 5:
+		velocity.y = JUMP_VELOCITY
+		
 	if position.distance_to(player_position) > 3:
-		var velocity = target_position * SPEED
-		move_and_slide(velocity)
+		velocity.x = target_position.x * SPEED
 	
-
-	## Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-#
-	## Get the input direction and handle the movement/deceleration.
-	## As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction := Input.get_axis("ui_left", "ui_right")
-	#if direction:
-		#velocity.x = direction * SPEED
-	#else:
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
+	sprite_flip(direction)
 
 	move_and_slide()
+
+func sprite_flip(direction):
+	if direction > 0:
+		sprite.flip_h = false
+	
+	elif direction < 0:
+		sprite.flip_h = true
